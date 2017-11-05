e68f0aded78aa9bd11e7df5100e336a2783c0e45
EDWALTER <ax@boxspring.ag>
2017-11-05 19:08:12 +0100

    03.11.2017

--- VirtualAssistentSystem.java
+++ VirtualAssistentSystem.java
@@ -1,21 +1,11 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class VirtualAssistentSystem {
 
-//    private static Communicator personA = new Communicator("A");
-//    private static Communicator personB = new Communicator("B");
-//    private static Communicator personC = new Communicator("C");
-//    private static Communicator universal = new Communicator("");
     private static ArrayList<String> drehBuch = new ArrayList<>();
-
     private static Map<String, Integer> objectMap = new HashMap<String, Integer>();
-
-
     private static String vergleichTxtEn="";
     private static String vergleichTxtDe="";
     private static String iam="G";
@@ -23,6 +13,7 @@ public class VirtualAssistentSystem {
     private static List<Communicator> personenListe = new ArrayList<Communicator>();
 
     public static void main (String[] args) throws IOException {
+
 
         if (args.length>0) {
             ladeDialog(args[0]);
@@ -36,35 +27,25 @@ public class VirtualAssistentSystem {
         else {
             ladeDialog("dialog");
              }
-        int a=0;
-        int b=0;
-        int c=0;
 
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+
+        while (auswahlPerson(bufferedReader)) { }
+
+
 
  //      System.out.println(iam);
 
         for (int idx=0; idx<drehBuch.size(); ++idx) {
 
-//if (drehBuch.get(idx).equals("A") && idx%2==0) { vergleichTxtEn = personA.getDialogENentry(a); vergleichTxtDe = personA.getDialogDEentry(a++);if (iam.equals("A")) {while (readInput(bufferedReader)) { }} else System.out.println( vergleichTxtEn);}
-//if (drehBuch.get(idx).equals("B") && idx%2==0) { vergleichTxtEn = personB.getDialogENentry(b); vergleichTxtDe = personB.getDialogDEentry(b++);if (iam.equals("B")) {while (readInput(bufferedReader)) { }} else System.out.println( vergleichTxtEn);}
-//if (drehBuch.get(idx).equals("C") && idx%2==0) { vergleichTxtEn = personC.getDialogENentry(c); vergleichTxtDe = personC.getDialogDEentry(c++);if (iam.equals("C")) {while (readInput(bufferedReader)) { }} else System.out.println( vergleichTxtEn + "*");}
-
-
 
 if (idx%2==0) {
 //    System.out.println(merke);
 
- //   System.out.println(drehBuch.get(idx));
-  //  System.out.println(personenListe.get(objectMap.get(drehBuch.get(idx))).getDialogENentry(idx));
-
         int m = objectMap.get(drehBuch.get(idx));
-    // if (personenListe.get(m).listDialogEN.size()>0) {
         if (personenListe.get(m).getDialogENsize()>0) {
-        // System.out.println(personenListe.get(m).getDialogENentry(0));
-
-
         vergleichTxtEn = personenListe.get(m).getDialogENentry(0);
         vergleichTxtDe = personenListe.get(m).getDialogDEentry(0);
 
@@ -83,36 +64,53 @@ if (idx%2==0) {
         System.out.println(personenListe.get(0).getDialogENsize());
 
               System.out.println(drehBuch);
-//        System.out.println(personenListe);
-        //       System.out.println(personenListe.get(0).getDialogENentry(0));
-//        System.out.println(personenListe.get(1).getDialogENentry(0));
-        //       System.out.println(personenListe.get(0).getDialogENentry(1));
-//        System.out.println(personenListe.get(0).listDialogEN);
-//        System.out.println(personenListe.get(1).listDialogEN);
-
               System.out.println(objectMap);
+        System.out.println(personenListe);
+        System.out.println(iam);
+    }
+
+    private static boolean auswahlPerson(BufferedReader bufferedReader) throws IOException {
+        System.out.println("Wer willst Du sein? Enter a number: " + objectMap);
+
+
+
+
+
+ //       System.out.println(nextLine.indexOf(0));
+ //       System.out.println(Integer.parseInt(nextLine));
+        String nextLine = bufferedReader.readLine();
+
+        char character = nextLine.charAt(0);
+        int c = (int) character;
+
+        System.out.println(c);
+
+ //       if (c > 49 && c < 58) { return false; }
+        if (c>57) {return true;}
+        if (c<49) {return true;}
+     //   else { return true; }
+
+        if (nextLine.isEmpty()) return true;
+
+        if (objectMap.containsValue(Integer.parseInt(nextLine))) {
+iam = personenListe.get(Integer.parseInt((nextLine))).getName();
+            System.out.println("Ok, you are: " + iam);
+            return false; }
+        else { return true; }
+
+
     }
 
     private static boolean readInput(BufferedReader bufferedReader) throws IOException {
-        // System.out.println(iam + "*" +vergleichTxtEn.substring(0,1));
 
         String[] parts = vergleichTxtEn.split(":");
-      //  System.out.println(parts[0]);
-      //  System.out.println(parts[1].trim());
-//        if (!iam.equals(vergleichTxtEn.substring(0,1))) { System.out.println(vergleichTxtEn);
-            if (!iam.equals(parts[0])) { System.out.println(vergleichTxtEn);
+       if (!iam.equals(parts[0])) { System.out.println(vergleichTxtEn);
 
  merke=true; return false; }
 
-
-//        System.out.println(vergleichTxtEn);
         String nextLine = bufferedReader.readLine();
 
- //       if (nextLine.equals(vergleichTxtEn.substring(3,vergleichTxtEn.length()-1)) || nextLine.equals(vergleichTxtEn.substring(3)) || nextLine.equals(">")) {
-//        if (nextLine.equals(vergleichTxtEn.substring(3,vergleichTxtEn.length()-1)) || nextLine.equals(vergleichTxtEn.substring(3))) {
-
-            if (nextLine.equals(parts[1].trim().substring(0,parts[1].length()-1)) || nextLine.equals(parts[1].trim())) {
-            // System.out.println("ok");
+            if (nextLine.equals(parts[1].trim().substring(0,parts[1].trim().length()-1)) || nextLine.equals(parts[1].trim())) {
             merke=true; return false;}
         if (nextLine.equals("exit")) { merke=false; return false;}
         if (nextLine.equals(">")) {
@@ -127,50 +125,32 @@ if (idx%2==0) {
 
     public static void ladeDialog(String file) {
 
-            File dialogTextFile = new File("C:/Users/noname/IdeaProjects/untitled/src/" + file + ".txt");
+            File dialogTextFile = new File("C:/Users/EDWALTER/IdeaProjects/untitled/src/" + file + ".txt");
             try (BufferedReader dialogTextReader = new BufferedReader(new InputStreamReader(new FileInputStream(dialogTextFile)))) {
                 String line;
                 int i = 0;
                 int xx =0;
                 while ((line = dialogTextReader.readLine()) != null) {
 
- //                   if (line.substring(0,1).equals("A")) { universal = personA; }
- //                   if (line.substring(0,1).equals("B")) { universal = personB; }
- //                   if (line.substring(0,1).equals("C")) { universal = personC; }
+                    String[] parts = line.split(":");
 
-String[] parts = line.split(":");
-
-//                    if (!drehBuch.contains(line.substring(0,1))) {
                     if (!drehBuch.contains(parts[0])) {
 
-             //           Communicator x = ;
-//                        personenListe.add(new Communicator(line.substring(0,1)));
                         personenListe.add(new Communicator(parts[0]));
-  //                      objectMap.put(line.substring(0,1), x.toString());
-//                        objectMap.put(line.substring(0,1), xx++);
                         objectMap.put(parts[0], xx++);
                     }
 
-
-             //       drehBuch.add(line.substring(0,1));
                     drehBuch.add(parts[0]);
 
 
 
                     ++i;
-                    //int tmp = objectMap.get(line.substring(0,1));
                     int tmp = objectMap.get(parts[0]);
-   //                 System.out.println(tmp);
                     if ((i % 2) == 0) {
-//                        System.out.println(i + " " + line + "##" + line.charAt(0));
- //                       universal.setDialogDE(line);
-
-            //            System.out.println(personenListe.get(personenListe.size() - 1));
 
                         personenListe.get(tmp).setDialogDE(line);
 
                     } else {
-//                        universal.setDialogEN(line);
                       personenListe.get(tmp).setDialogEN(line);
 
                     }
